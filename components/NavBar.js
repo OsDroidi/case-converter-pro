863b7e343b0e24513cd8847a943e0db5d5597c86
Osama Saeed <osdroidi@gmail.com>
2022-01-09 22:40:59 +0300

    fix: small isuues

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,12 +3,14 @@ import { Toggle } from "./Toggle";
 
 const NavBar = () => {
   return (
-    <div className="nav-bar">
+    <>
+      <div className="nav-bar">
+        <p className="pretty-text-underline">About</p>
+        <p className="pretty-text-underline">Contact</p>
+        <p className="pretty-text-underline">Learn</p>
+      </div>
       <Toggle />
-      <p className="pretty-text-underline">About</p>
-      <p className="pretty-text-underline">Contact</p>
-      <p className="pretty-text-underline">Learn</p>
-    </div>
+    </>
   );
 };
 
