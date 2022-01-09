14366a5a22467ce2ea04a71387adaecd6821c860
Osama Saeed <osdroidi@gmail.com>
2022-01-09 13:44:09 +0300

    fix: CSS :focus Not Working in iOS ontouchstart

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head></Head>
-        <body>
+        <body ontouchstart="">
           <script
             dangerouslySetInnerHTML={{
               __html: themeInitializerScript,
