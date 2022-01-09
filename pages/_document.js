1c54ac9684f943ec093494b7ba7a94b4d58caf35
Osama Saeed <osdroidi@gmail.com>
2022-01-09 13:46:59 +0300

    fix: onTouchStart

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head></Head>
-        <body ontouchstart="">
+        <body onTouchStart="">
           <script
             dangerouslySetInnerHTML={{
               __html: themeInitializerScript,
